640f6936f396680b99f5e8dc014c06a36e313cd6
Ahmad al-aswad <ahmad.alaswad.dev@gmail.com>
2023-10-22 02:15:47 +0300

    10/22

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -8,28 +8,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Astrotomic\Translatable\Translatable;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 
-class Notification extends DatabaseNotification 
+class Notification extends DatabaseNotification implements TranslatableContract
 {
     use HasFactory,Translatable;
-
     protected $casts = [
         'read_at' => 'datetime',
     ];
-    // protected $with = ['translations'];
-
-    // public function translations()
-    // {
-    //     return $this->hasMany(NotificationTranslation::class);
-    // }
-    
-
-
-    // public function getTranslation()
-    // {
-    //     $currentLocale = app()->getLocale();
-    //     return $this->translations->where('locale', 'en')->first();
-    // }
-    
-
+    protected $with = ['translations'];
+    public $translatedAttributes = ['data'];
 
 }
